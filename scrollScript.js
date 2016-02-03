e0436edd1c61deb3de4670e892a465395cd7e7ac
Cody Clawson <cclawson@student.neumont.edu>
2016-02-03 15:50:19 -0700

    added down arrow on first page

--- scrollScript.js
+++ scrollScript.js
@@ -1,11 +1,25 @@
-$('.nav li a').click(function(event) {
+$('.nav li a').click(function (event) {
     event.preventDefault();
     var navOffset = $('.navbar').height();
     console.log(navOffset);
     var hash = this.hash;
     console.log($(hash).offset().top);
     $('html, body').animate({
-        scrollTop: $(hash).offset().top - navOffset}, 300, function() {
-        window.location.hash = hash; 
-    });            
+        scrollTop: $(hash).offset().top - navOffset
+    }, 300, function () {
+        window.location.hash = hash;
+    });
+});
+
+$('#downarrow a').click(function (event) {
+    event.preventDefault();
+    var navOffset = $('.navbar').height();
+    console.log(navOffset);
+    var hash = this.hash;
+    console.log($(hash).offset().top);
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top - navOffset
+    }, 300, function () {
+        window.location.hash = hash;
+    });
 });
