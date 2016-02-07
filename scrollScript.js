2247042efe893a5697436b4efbe22b8504de5fb9
Emerson Roberts <eroberts@student.neumont.edu>
2016-02-07 15:17:02 -0700

    made logo scroll on click, added a stylesheet for about div, added jumbotron to about div

--- scrollScript.js
+++ scrollScript.js
@@ -23,3 +23,15 @@ $('#downarrow a').click(function (event) {
         window.location.hash = hash;
     });
 });
+$('#pageLogo').click(function (event) {
+    event.preventDefault();
+    var navOffset = $('.navbar').height();
+    console.log(navOffset);
+    var hash = this.hash;
+    console.log($(hash).offset().top);
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top - navOffset
+    }, 300, function () {
+        window.location.hash = hash;
+    });
+});
