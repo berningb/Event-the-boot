ec42aaecf27934e5bb0140f9d9fe6e88d2a8b648
Emerson Roberts <eroberts@student.neumont.edu>
2016-02-07 15:41:15 -0700

    carousel links now scroll as well

--- scrollScript.js
+++ scrollScript.js
@@ -1,3 +1,15 @@
+$('#pagelink').click(function (event) {
+    event.preventDefault();
+    var navOffset = $('.navbar').height();
+    console.log(navOffset);
+    var hash = this.hash;
+    console.log($(hash).offset().top);
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top - navOffset
+    }, 300, function () {
+        window.location.hash = hash;
+    });
+});
 $('.nav li a').click(function (event) {
     event.preventDefault();
     var navOffset = $('.navbar').height();
