82e49fd19dee9d79e689ee3802b4ec62dc1a7199
Cody Clawson <cclawson@student.neumont.edu>
2016-02-08 11:16:16 -0700

    a

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ $(document).mousemove(function (e) {
     mouseX = e.pageX;
     mouseY = e.pageY - 220;
 });
-$("#hover").mouseover(function () {
+$(".hover").on("click", function () {
     $('#tobeshown').css({
+        'height': "250px",
+        'width': "300px",
         'top': mouseY,
         'left': mouseX,
         'background-color': "yellow"
-    }).fadeIn("slow");
+    }).fadeIn("fast");
 });
 
-$("#hover").mouseout(function () {
+$(".hover").mouseout(function () {
     $('#tobeshown').fadeOut('slow');
 });
