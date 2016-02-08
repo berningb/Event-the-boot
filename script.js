46280ea3d2dddd263048a43c62a83f8d78d8fa63
brandon berning <brandon berning>
2016-02-08 13:45:52 -0700

    fixed the lsit

--- script.js
+++ script.js
@@ -2,15 +2,12 @@ var mouseX;
 var mouseY;
 $(document).mousemove(function (e) {
     mouseX = e.pageX;
-    mouseY = e.pageY - 220;
+    mouseY = e.pageY - 300;
 });
 $(".hover").on("click", function () {
     $('#tobeshown').css({
-        'height': "250px",
-        'width': "300px",
         'top': mouseY,
         'left': mouseX,
-        'background-color': "yellow"
     }).fadeIn("fast");
 });
 
